6c8d981d8d754900fa099553ae959365be0ffa9e
${Mr.DJA} <42304709+iMrDJAi@users.noreply.github.com>
2020-03-26 18:16:12 +0100

    Update markdown.js

--- archive/DZGC.MARKDOWN-RENDERER/markdown.js
+++ archive/DZGC.MARKDOWN-RENDERER/markdown.js
@@ -116,7 +116,7 @@
         }
         if (n === 2) { //^supscript ~subscript
             return window.markdownitRegexp(
-                /([\^|\~])(\S+)/,
+                /([\^|\~])((?:\[[^\]]*\]\([^)]*\)|[\S])+)/,
                 function (match, utils) {
                     if (match[1] === '^') {
                         const html = inlineRenderer('supsubscript').render(match[2], env);
